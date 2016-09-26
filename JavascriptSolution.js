849f9934eb47af38ab74b331e59ef8dfa4c7c0d1
Pat Hiniker <pathiniker@Pats-MacBook-Pro.local>
2016-09-26 11:33:39 -0500

    added completed javascript assignment

---
+++ JavascriptSolution.js
@@ -0,0 +1,64 @@
+
+//Problem 1:
+michelangelo(theNumber) + leonardo(theNumber);
+// 2 + 5;
+// 7
+
+//michelangelo(theNumber);
+//theNumber = 1
+//value = (value *2);
+//value = (1 * 2);
+//value = 2;
+//return value;
+
+
+//leonardo(theNumber);
+//theNumber = 1
+//value = theNumber
+//value = 1
+//anotherValue = value
+//anotherValue = 1
+//value *= 4;
+//value = 4;
+//theNumber = value / 2;
+//theNumber = 2;
+//return (value + anotherValue);
+//return (4 + 1)
+//return (5)
+
+theNumber = theNumber * 2;
+
+//theNumber = 2;
+//theNumber = theNumber * 2;
+//theNumber = 2 * 2;
+//theNumber = 4;
+
+//Problem 2:
+(donatello(theNumber, 2)) - (raphael(3, 2, theNumber));
+// 12 - 4 = 8
+
+
+//(donatello(theNumber, 2));
+//theNumber = 4 **assuming we are continuing from our last expression**
+//(donatello(4, 2));
+//value = 4;
+//anotherValue = 2;
+//value = anotherValue + value;
+//value = 2 + 4;
+//value = 6;
+//anotherValue = value * anotherValue;
+//anotherValue = 6 * 2;
+//anotherValue = 12;
+//return anotherValue;
+
+//(raphael(3, 2, theNumber));
+//value = 3
+//anotherValue = 2
+//yetAnotherValue = theNumber
+//yetAnotherValue = 4
+//value = value * anotherValue + yetAnotherValue
+// value = 3 * 2 + 4;
+// value = 6 + 4
+ // value = 10
+ // return yetAnotherValue;
+ // return 4;
